593fff5a980a80fd6008e39a365ecb4b3f97be0d
lopamudradas19 <lopamudradas19012001@gmail.com>
2023-06-07 08:27:18 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ram(request):
+    return render(request,'ram.html')
+
+def Hari(request):
+    return render(request,'Hari.html')
